fbc03e5ea8e20f3c6c02beefade5e403c6d13528
nkauvmaalr <73358495+nkauvmaalr@users.noreply.github.com>
2021-05-26 13:22:26 +0530

    gfdfdh

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
         CartEntry cartEntryDomain = CartEntryDomain.getcartResponse();
-
         System.out.println(new Gson().toJson(cartEntryDomain));
     }
 }
